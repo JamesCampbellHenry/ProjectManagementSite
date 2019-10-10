b168c7c264d17e9344bbc6774bdb6fe3bd243bf1
frostdog1 <55537341+frostdog1@users.noreply.github.com>
2019-10-10 21:38:21 +0100

    Add files via upload

--- managestaff.php
+++ managestaff.php
@@ -9,6 +9,8 @@
 
 	// include functions to dump blocks of HTML
 	include 'flatText.php';
+	
+	include 'manage_staff.html';
 
 	// function places everything from <!DOCTYPE html> to </head>
 	getHeader('Manage Staff');
@@ -20,9 +22,66 @@
     <?PHP
   		// function places the nav bar on the page
   		getNavBar('manage_staff');
+		
+		
   	?>
-
-
+	
+	<div class="container-fluid">
+	
+		<form class="staffInput" action="#" style="margin-top: 100px;" id="customFormSize">
+		
+			<div class="form-row">
+			
+				<div class="form-group col-md-6">
+				
+					<label for="fname">First Name</label>
+				  
+					<input type="text" class="form-control" id="fname" required>
+					
+				</div>
+				
+				
+				<div class="form-group col-md-6">
+				
+					<label for="lname">Last Name</label>
+					
+					<input type="text" class="form-control" id="lname" required>
+					
+				</div>
+				
+				
+			</div>
+			
+			<div class="form-group">
+			
+				<label for="staffEmail">Email</label>
+				
+				<input type="email" class="form-control" id="staffEmail" placeholder="you@example.com" required>
+				
+			</div>
+			
+			<div class="form-group">
+			
+				<label for="staffPassword">Password</label>
+				
+				<input type="password" class="form-control" id="staffPassword" required>
+				
+			</div>
+			
+			<div class="form-group">
+			
+				<label for="staffBio">Biography</label>
+				
+				<textarea class="form-control" id="staffBio" rows="2" placeholder="Experience / Skills"></textarea>
+				
+			</div>
+			
+			<!-- button is styled in css to match the RGU purple theme -->
+			<button type="submit" class="btn btn-primary" id="staffInputBtn">Sign Up</button>
+			
+		</form>
+	
+	</div>
 
 </body>
 
